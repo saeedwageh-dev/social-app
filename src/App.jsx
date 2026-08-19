c7735a47aa088f9ab834c8c9cbb24ef3d9bc79dc
saeed wageh <s94391554@gmail.com>
2026-08-19 05:45:41 +0200

    second edit for refresh

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,51 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
 import Layout from "./Layout/Layout";
+
 import Home from "./Pages/Home/Home";
 import Profile from "./Pages/Profile/Profile";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import PostDetails from "./Pages/PostDetails/PostDetails";
+
 import Auth from "./Pages/Auth/AuthPage";
 import Login from "./Pages/Auth/Login/Login";
 import Register from "./Pages/Auth/Register/Register";
+
 import { AuthContextProvider } from "./Context/AuthContext";
+
 import ProtectedRoute from "./components/ProtectedRoute/ProtectedRoute";
 import ProtectedAuth from "./components/ProtectedAuth/ProtectedAuth";
+
 import PageNotFound from "./Pages/PageNotFound/PageNotFound";
 
 const queryClient = new QueryClient();
 
 function App() {
-  const route = createBrowserRouter([
+  const router = createBrowserRouter([
+    // Authentication routes
     {
       path: "/login",
       element: (
         <ProtectedAuth>
-          <Auth />
-        </ProtectedAuth>
-      ),
-    },
-    {
-      path: "/register",
-      element: (
-        <ProtectedAuth>
-          <Auth />
+          <Auth>
+            <Login />
+          </Auth>
         </ProtectedAuth>
       ),
     },
 
+    {
+      path: "/register",
+      element: (
+        <ProtectedAuth>
+          <Auth>
+            <Register />
+          </Auth>
+        </ProtectedAuth>
+      ),
+    },
+
+    // Protected application routes
     {
       element: <ProtectedRoute />,
       children: [
@@ -44,31 +57,31 @@ function App() {
               element: <Home />,
             },
             {
-              path: "profile",
+              path: "/profile",
               element: <Profile />,
             },
             {
-              path: "PostDetails/:id",
+              path: "/PostDetails/:id",
               element: <PostDetails />,
             },
           ],
         },
       ],
     },
+
+    // 404
     {
       path: "*",
       element: <PageNotFound />,
-    }
+    },
   ]);
 
   return (
-    <>
-        <QueryClientProvider client={queryClient}>
+    <QueryClientProvider client={queryClient}>
       <AuthContextProvider>
-          <RouterProvider router={route} />
+        <RouterProvider router={router} />
       </AuthContextProvider>
-        </QueryClientProvider>
-    </>
+    </QueryClientProvider>
   );
 }
 
